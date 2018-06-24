ae846a47d272ad12c9e2cb9ce319145053a00645
wlan0 <sidharthamn@gmail.com>
2018-06-24 12:07:21 -0700

    boilerplate

---
+++ cmd/compiler/concerto.go
@@ -0,0 +1,46 @@
+package compiler
+
+import (
+	"flag"
+
+	"github.com/golang/glog"
+	"github.com/spf13/cobra"
+)
+
+var (
+	RootCmd = &cobra.Command{
+		Use:   "concerto",
+		Short: "DevOps Programming Language",
+		Long: `DevOps Programming Language
+
+Full documentation available at https://docs.koki.io/concerto
+`,
+		RunE: func(c *cobra.Command, args []string) error {
+			return concerto(c, args)
+		},
+		SilenceUsage: true,
+		Example: `
+
+  # Compile a concerto program
+  concerto redis.concerto
+`,
+	}
+)
+
+func init() {
+	// parse the go default flagset to get flags for glog and other packages in future
+	RootCmd.PersistentFlags().AddGoFlagSet(flag.CommandLine)
+
+	// defaulting this to true so that logs are printed to console
+	flag.Set("logtostderr", "true")
+
+	//suppress the incorrect prefix in glog output
+	flag.CommandLine.Parse([]string{})
+
+	RootCmd.AddCommand(versionCmd)
+}
+
+func concerto(c *cobra.Command, args []string) error {
+	glog.Info("Compiling...")
+	return nil
+}
